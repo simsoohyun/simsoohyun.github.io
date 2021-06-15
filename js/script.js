252481ac59e316058c6c8558a26d880c3013a65d
simsoohyun <78782897+simsoohyun@users.noreply.github.com>
2021-06-16 00:48:18 +0900

    0615 work done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 'use strict';
+$(function(){
+    var $header = $('#header'),
+        $headerSmall = $('#header__small'),
+        $logo = $('.logo'),
+        $gnb = $('.gnb'),
+        $arrowup = $('#arrowup'),
+        $home = $('#home');
+
+    $(window).scroll(function(){
+        var scrollTop = $(window).scrollTop();
+        var headerHeight = $(header).outerHeight();
+        var homeHeight = $(home).outerHeight();
+        /* 스크롤이 header보다 내려가면 header가 fix 됨 */
+        if(scrollTop > headerHeight){
+            $headerSmall.addClass('fixed');
+            $logo.addClass('fixed');
+            $gnb.addClass('fixed');
+        }else {
+            $headerSmall.removeClass('fixed');
+            $logo.removeClass('fixed');
+            $gnb.removeClass('fixed');
+        }
+        /* 스크롤이 home보다 내려가면 arrowup이 보이게 됨 (미완.. 안됨 ㅜㅜ)*/
+        if(scrollTop > homeHeight){
+            $arrowup.addClass('visible');
+        }else {
+            $arrowup.removeClass('visible');
+        }
+    })
+
+    /* arrowup을 클릭하면 화면이 위로 올라가게 함 */
+    $(arrowup).click(function(){
+        $('html, body').animate({scrollTop: 0});
+    });
+})
